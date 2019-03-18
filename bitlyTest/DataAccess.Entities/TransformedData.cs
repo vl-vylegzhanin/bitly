f3b0d0a45c8cefc6f3d6b2550f4b325030597c60
Vladimir <vladmir.vylegzhanin@freemake.com>
2019-03-18 11:39:31 +0300

    remove legacy assemblies

--- bitlyTest/DataAccess.Entities/TransformedData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccess.Entities
-{
-    public class TransformedData
-    {
-        public string OriginalUrl { get; set; }
-        public string TrimmedUrl { get; set; }
-        public int RedirectsCount { get; set; }
-    }
-}
